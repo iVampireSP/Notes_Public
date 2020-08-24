8e23a94e4aa4d90ee7f04083afa2196e2a04a710
iVampireSP.com <im@ivampiresp.com>
2020-08-24 22:39:48 +0800

    优化拉取

--- api/pull.php
+++ api/pull.php
@@ -6,21 +6,27 @@ $type = htmlspecialchars(urlencode($_REQUEST['type']));
 if (empty($type)) {
     $type = 'plain';
 }
-header("Content-Type: text/$type");
+
 $user = new User();
 $user->db_con = $db_con;
 $user->noteid = $_REQUEST['id'];
 switch ($_REQUEST['action']) {
     case 'getTitle':
+        header("Content-Type: text/$type");
         echo $user->getTitle();
     break;
 
     case 'getContent':
+        header("Content-Type: text/$type");
         echo $user->viewNote();
     break;
 
     default:
-    echo $user->getTitle() . PHP_EOL;
-    echo $user->viewNote();
+    header('Content-Type:application/json; charset=utf-8');
+    $array = array(
+        'title' => $user->getTitle(),
+        'content' => $user->viewNote()
+    );
+    print_r(json_encode($array));
 break;
 }
