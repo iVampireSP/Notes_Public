e96555eb0f8fa8ee9230445287a75a13f6415123
iVampireSP.com <im@ivampiresp.com>
2020-08-26 16:48:38 +0800

    删除多余代码

--- shareground.php
+++ shareground.php
@@ -10,8 +10,7 @@ require_once('config/theme.php');
 // $user->addNote('标题', '内容', NULL, $db_con);
 // 翻页
 ?>
-<ul class="mdui-list">
-</ul>
+<style type="text/css">.mdui-list-item,.mdui-list-item .mdui-ripple {border-radius: 10px;}</style>
 <!-- 别问我什么在标题标签里面套button，因为他居中一直可以的 -->
 <h5 style="text-align:center"><button id="loadMore-btn" onclick="loadMore()" class="mdui-btn mdui-btn-raised mdui-btn-dense mdui-color-theme-accent mdui-ripple">加载更多</button></h5>
 <div id="loadMore-btn" style="width: 163px; height: 20px; margin: 0px auto;"><span id="loadMore-btn" style="color: gray;">Powered&nbsp;By&nbsp;Loli.Rocks&nbsp;Team.</span></div>
